767f491ccc2563ee0700ce61fdb6d877831efa16
Grant Cummings <grantcummings4429@gmail.com>
2024-07-31 08:09:02 -0400

    add: fibonacci recursion

--- TwoPointers/fibonacci.js
+++ TwoPointers/fibonacci.js
@@ -13,9 +13,17 @@ function fibonacciIterative(n){
     return fibArray[n]
 }
 
-console.log(fibonacciIterative(9))
+
 
 
 function fibonacciRecursive(n){
-
+    if (n === 0){
+        return 0
+    } 
+    if (n === 1){
+        return 1
+    }
+    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
 }
+
+console.log(fibonacciRecursive(5))
