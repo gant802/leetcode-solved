04b92537a7191246f16715e289331db1d1c65bcf
Grant Cummings <grantcummings4429@gmail.com>
2024-08-05 13:47:48 -0400

    fixed maxSubarraySum bug

--- Arrays/maxSubarraySum.js
+++ Arrays/maxSubarraySum.js
@@ -1,13 +1,22 @@
-function maxSubarraySum(nums, size){
-    let currSum = 0
-    let maxSumSeen = -Infinity
+function maxSubarraySum(nums, size) {
+    let currSum = 0;
+    let maxSumSeen = -Infinity;
 
-    for (let i = 0; i < nums.length; i++){
+    // Initialize the current sum with the first 'size' elements
+    for (let i = 0; i < size; i++) {
         currSum += nums[i];
-        if(i > size - 1){
-            maxSumSeen = Math.max(currSum, maxSumSeen)
-            currSum -= nums[i - (size - 1)]
-        }
     }
-    return maxSumSeen
+    maxSumSeen = currSum; // Start with the sum of the first 'size' elements
+
+    // Slide the window over the rest of the elements
+    for (let i = size; i < nums.length; i++) {
+        currSum += nums[i] - nums[i - size]; // Update the current sum
+        maxSumSeen = Math.max(currSum, maxSumSeen); // Update the max sum seen
+    }
+
+    return maxSumSeen;
 }
+
+//Given an integer array return the max subarray of the inputted size 
+
+console.log(maxSubarraySum([1,2,3,4,5], 2))
