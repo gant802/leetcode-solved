c4f192bd6e89c1b2073463a2decd2a7cf772dd1a
Grant Cummings <grantcummings4429@gmail.com>
2024-08-05 13:44:53 -0400

    add: maxSubarraySum

--- Arrays/subarraySum.js
+++ Arrays/subarraySum.js
@@ -15,6 +15,23 @@ var subarraySum = function(nums, k) {
     return count;
 };
 
+// function subarraySum(nums, size){
+//     let currSum = 0
+//     let maxSumSeen = -Infinity
+
+//     for (let i = 0; i < nums.length; i++){
+//         currSum += nums[i];
+//         if(i > size - 1){
+//             maxSumSeen = Math.max(currSum, maxSumSeen)
+//             currSum -= nums[i - (size - 1)]
+//         }
+//     }
+//     return maxSumSeen
+// }
+
+console.log(subarraySum([1,2,3,5,2,4,3,6,7,8,6], 3))
+
+
 // Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 
 // A subarray is a contiguous non-empty sequence of elements within an array.
